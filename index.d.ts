55ce4d2ea4158cbe338cafbd3f47d8ff60c95209
Yingkaixiang <rubykaikai@gmail.com>
2019-03-26 11:55:13 +0800

    fix: fix index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,34 +1,38 @@
-import React from "react";
+import * as React from "react";
 
-export interface Element {
-  constructor(opts?: any);
-  shape: any;
-  style: any;
-  type: string;
-  setShape(opts: any): void;
-  setStyle(opts: any): void;
-}
+// export interface Element {
+//   constructor(opts?: any);
+//   shape: any;
+//   style: any;
+//   type: string;
+//   setShape(opts: any): void;
+//   setStyle(opts: any): void;
+// }
 
-export interface Group extends Element {
-  id: number;
-  add(child: Element): void;
-  remove(child: Element): void;
-  children(): Element[];
-  childOfName(name: string): Element[];
-}
+// export interface Group extends Element {
+//   id: number;
+//   add(child: Element): void;
+//   remove(child: Element): void;
+//   children(): Element[];
+//   childOfName(name: string): Element[];
+// }
 
-export interface CanvasProps {
-  width: number | string;
-  height: number | string;
-  color?: string;
-  beforeDraw?: (event: MouseEvent) => boolean;
-  onMove?: (event: MouseEvent) => void;
-  onComplete?: (group: Group) => void;
-  onUndo?: (group: Group) => void;
-  type?: "polygon" | "line";
+export type PolyBrush2DType = "polygon" | "line";
+
+export interface PolyBrush2DProps {
+  width?: number | string;
+  height?: number | string;
   background?: string;
+  color?: string;
+  errorColor?: string;
+  type?: any;
+  logging?: boolean;
+  onBeforeDraw?: (event: MouseEvent) => boolean;
+  onMove?: (event: MouseEvent) => void;
+  onComplete?: (group: any) => void;
+  onUndo?: (group: any) => void;
 }
 
-declare class Polybrush extends React.Component<CanvasProps, any> {}
+declare class Polybrush2D extends React.Component<PolyBrush2DProps, any> {}
 
-export default Polybrush;
+export default Polybrush2D;
